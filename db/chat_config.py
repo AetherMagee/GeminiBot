7e9c7dbe1e3060e123e9a2292b62b745c9a9fed8
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-07-27 00:09:02 +0300

    funny

--- db/chat_config.py
+++ db/chat_config.py
@@ -1,7 +1,7 @@
 import db.shared as dbs
 
 
-async def _create_chat_config_entry_if_not_exists(chat_id: int):
+async def __create_telegram_chat_config_if_does_not_exist_in_database_postgres(chat_id: int):
     async with dbs.pool.acquire() as conn:
         check_result = await conn.fetch("SELECT * FROM chat_config WHERE chat_id = $1", chat_id)
         if not check_result:
@@ -9,7 +9,7 @@ async def _create_chat_config_entry_if_not_exists(chat_id: int):
 
 
 async def get_chat_parameter(chat_id: int, parameter_name: str):
-    await _create_chat_config_entry_if_not_exists(chat_id)
+    await __create_telegram_chat_config_if_does_not_exist_in_database_postgres(chat_id)
     async with dbs.pool.acquire() as conn:
         result = await conn.fetchrow(f"SELECT {parameter_name} FROM chat_config WHERE chat_id = {chat_id}")
         return list(result)[0]
