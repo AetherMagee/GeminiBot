56970f43d62e04666c47f0f4ebccb00e69bcd222
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-07-27 15:07:33 +0300

    fix: Inability to fetch messages for a new chat

--- db/messages/get.py
+++ db/messages/get.py
@@ -1,13 +1,18 @@
-from asyncpg import Record
+from asyncpg import Record, UndefinedTableError
 
 import db
 import db.shared as dbs
+from db.table_creator import create_message_table
 
 
 async def get_messages(chat_id: int) -> list[Record]:
     async with dbs.pool.acquire() as conn:
-        results = await conn.fetch(f"SELECT * FROM messages{await dbs.sanitize_chat_id(chat_id)} "
-                                   f"WHERE deleted=false ORDER BY timestamp "
-                                   f"LIMIT {await db.get_chat_parameter(chat_id, 'message_history_limit')}")
+        try:
+            results = await conn.fetch(f"SELECT * FROM messages{await dbs.sanitize_chat_id(chat_id)} "
+                                       f"WHERE deleted=false ORDER BY timestamp "
+                                       f"LIMIT {await db.get_chat_parameter(chat_id, 'message_history_limit')}")
 
-        return results
+            return results
+        except UndefinedTableError:
+            await create_message_table(conn, await dbs.sanitize_chat_id(chat_id))
+            await get_messages(chat_id)
