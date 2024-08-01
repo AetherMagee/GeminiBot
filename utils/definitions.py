a35ba5dbfe6ed7c90d7915447fb41202e18712ca
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-01 10:15:39 +0300

    fix(google.api): Decrease default and max context length

--- utils/definitions.py
+++ utils/definitions.py
@@ -2,8 +2,8 @@ chat_configs = {
     "message_limit": {
         "description": "Максимум сообщений в памяти бота",
         "type": "integer",
-        "default_value": 1000,
-        "accepted_values": range(1, 5000)
+        "default_value": 500,
+        "accepted_values": range(1, 3000)
     },
     "reset_permission": {
         "description": "Кто может пользоваться командой /reset",
