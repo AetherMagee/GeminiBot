297b124c3f44e6ecf234edb3aff68c4ac863de36
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-02 17:27:57 +0300

    refactor(api.google): Move prompt and error handling out of generate_response

--- api/google/google.py
+++ api/google/google.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import random
-from typing import Union
+from typing import List, Union
 
 import google.generativeai as genai
 from aiogram.types import Message
@@ -19,6 +19,14 @@ api_keys = os.getenv("GEMINI_API_KEYS").split(", ")
 api_key_index = 0
 with open(os.getenv("DEFAULT_SYSTEM_PROMPT_FILE_PATH"), "r") as f:
     default_prompt = f.read()
+MAX_API_ATTEMPTS = 3
+ERROR_MESSAGES = {
+    "unsupported_file_type": "❌ *Данный тип файлов не поддерживается Gemini API.*",
+    "censored": "❌ *Запрос был заблокирован цензурой Gemini API.*",
+    "unknown": "❌ *Произошел сбой Gemini API.",
+    "system_failure": "Your response was supposed to be here, but you failed to reply for some reason. Be better next "
+                      "time."
+}
 
 
 def _get_api_key() -> str:
@@ -27,21 +35,8 @@ def _get_api_key() -> str:
     return api_keys[api_key_index % len(api_keys)]
 
 
-def get_available_models() -> list:
-    logger.info("Getting available models...")
-    genai.configure(api_key=_get_api_key())
-    models = genai.list_models()
-    model_list = []
-    hidden = ["bison", "aqa", "embedding", "gecko"]
-    for model in models:
-        if not any(hidden_word in model.name for hidden_word in hidden):
-            model_list.append(model.name.replace("models/", ""))
-
-    return model_list
-
-
-async def _call_gemini_api(request_id: int, prompt: list, max_attempts: int, token: str, model_name: str) -> Union[
-    AsyncGenerateContentResponse, str, None]:
+async def _call_gemini_api(request_id: int, prompt: list, token: str, model_name: str) \
+        -> Union[AsyncGenerateContentResponse, str, Exception]:
     safety = {
         "SEXUALLY_EXPLICIT": "block_none",
         "HARASSMENT": "block_none",
@@ -51,21 +46,18 @@ async def _call_gemini_api(request_id: int, prompt: list, max_attempts: int, tok
 
     logger.debug(f"{request_id} | Using model {model_name}")
 
-    attempts = 0
-    while attempts < max_attempts:
-        attempts += 1
-        logger.debug(f"{request_id} | Generating, attempt {attempts}")
+    model = genai.GenerativeModel(model_name)
 
-        model = genai.GenerativeModel(model_name)
-
+    for attempt in range(1, MAX_API_ATTEMPTS + 1):
+        logger.debug(f"{request_id} | Generating, attempt {attempt}")
         try:
             response = await model.generate_content_async(prompt, safety_settings=safety)
             return response
         except InvalidArgument:
-            return "❌ <b>Такие файлы не поддерживаются Gemini API.</b>"
+            return ERROR_MESSAGES["unsupported_file_type"]
         except Exception as e:
             logger.error(f"{request_id} | Error \"{e}\" on key: {token}")
-            if attempts == max_attempts - 1:
+            if attempt == MAX_API_ATTEMPTS:
                 return e
 
     return None
@@ -93,6 +85,69 @@ async def _format_message_for_prompt(message: Record) -> str:
     return result
 
 
+async def _prepare_prompt(message: Message, chat_messages: List[Record]) -> List[Union[str, dict]]:
+    all_messages_list = [await _format_message_for_prompt(msg) for msg in chat_messages]
+    all_messages = "\n".join(all_messages_list)
+
+    photos = [await get_photo(message)]
+    photos = photos if photos[0] else []
+    additional_media = await get_other_media(message, _get_api_key())
+
+    chat_type = "direct message (DM)" if message.from_user.id == message.chat.id else "group"
+    chat_title = f" called {message.chat.title}" if message.from_user.id != message.chat.id else f" with {message.from_user.first_name}"
+
+    media_warning = (
+        "\n- Your target message or a message related to it happen to contain some media files. "
+        "They have been attached for your analysis. When working with these files, follow these "
+        "rules: 1) If you are sure that it wasn't described before in the chat history, "
+        "describe PERFECTLY and AS THOROUGHLY AS POSSIBLE whatever is contained in the mediafile. "
+        "You won't be able to see it again, and the User might ask additional questions, "
+        "so these notes will also function as your future guidelines. When describing, start with "
+        "\"This <media_type> contains\" in the User's language. For example, when working with an "
+        "image and talking in a Russian -speaking chat, start with \"Это изображение содержит\". 2) "
+        "Perform what the user asks you to do. So, if it's an image and the User wants the text on "
+        "it - say it. If it's an audio resembling a voice message - transcript it as accurately as "
+        "possible, then handle it as a normal message directed at you. So, if it has any "
+        "instructions for you - execute them. Otherwise, ask the User what they want exactly."
+    ) if photos or additional_media else None
+
+    prompt = default_prompt.format(
+        chat_type=chat_type,
+        chat_title=chat_title,
+        all_messages=all_messages,
+        target_message=all_messages_list[-1],
+        media_warning=media_warning
+    )
+
+    return [prompt] + photos + additional_media
+
+
+async def _handle_api_response(
+        request_id: int,
+        response: Union[AsyncGenerateContentResponse, str, Exception, None],
+        message: Message,
+        store: bool,
+        show_error_message: bool
+) -> str:
+    if isinstance(response, AsyncGenerateContentResponse):
+        output = response.text.replace("  ", " ")
+    elif isinstance(response, str):
+        output = response
+    elif isinstance(response, Exception):
+        error_message = (": " + str(response)) if show_error_message else ""
+        output = ERROR_MESSAGES["unknown"].format(error_message)
+    else:
+        logger.error(f"{request_id} | Unexpected response type: {type(response)}")
+        if store:
+            await db.save_system_message(message.chat.id, ERROR_MESSAGES["system_failure"])
+        output = ERROR_MESSAGES['unknown'].format("")
+
+    if not output.startswith("❌") and store:
+        await db.save_our_message(message, output)
+
+    return output
+
+
 async def generate_response(message: Message) -> str:
     request_id = random.randint(100000, 999999)
     token = _get_api_key()
@@ -100,63 +155,32 @@ async def generate_response(message: Message) -> str:
     message_text = message.text if message.text else message.caption
 
     logger.debug(
-        f"RID: {request_id} | UID: {message.from_user.id} | CID: {message.chat.id} | MID: {message.message_id}")
+        f"RID: {request_id} | UID: {message.from_user.id} | CID: {message.chat.id} | MID: {message.message_id}"
+    )
 
     chat_messages = await db.get_messages(message.chat.id)
-    all_messages_list = [await _format_message_for_prompt(message) for message in chat_messages]
-    all_messages = "\n".join(all_messages_list)
-
     typing_task = asyncio.create_task(simulate_typing(message.chat.id))
 
-    photos = [await get_photo(message)]
-    if not photos[0]:
-        photos = []
-    additional_media = await get_other_media(message, token)
-
     if not message_text.startswith("/raw"):
-        prompt = default_prompt
-        prompt = prompt.format(
-            chat_type="direct message (DM)" if message.from_user.id == message.chat.id else "group",
-            chat_title=f" called {message.chat.title}" if message.from_user.id != message.chat.id else f" with {message.from_user.first_name}",
-            all_messages=all_messages,
-            target_message=all_messages_list[-1],
-            media_warning="\n- Your target message or a message related to it happen to contain some media files. "
-                          "They have been attached for your analysis. When working with these files, follow these "
-                          "rules: 1) If you are sure that it wasn't described before in the chat history, "
-                          "describe PERFECTLY and AS THOROUGHLY AS POSSIBLE whatever is contained in the mediafile. "
-                          "You won't be able to see it again, and the User might ask additional questions, "
-                          "so these notes will also function as your future guidelines. When describing, start with "
-                          "\"This <media_type> contains\" in the User's language. For example, when working with an "
-                          "image and talking in a Russian-speaking chat, start with \"Это изображение содержит\". 2) "
-                          "Perform what the user asks you to do. So, if it's an image and the User wants the text on "
-                          "it - say it. If it's an audio resembling a voice message - transcript it as accurately as "
-                          "possible, then handle it as a normal message directed at you. So, if it has any "
-                          "instructions for you - execute them. Otherwise, ask the User what they want exactly." if
-            photos or additional_media else None
-        )
+        prompt = await _prepare_prompt(message, chat_messages)
         store = True
     else:
-        prompt = message_text.replace("/raw ", "", 1)
-        if "--dont-store" in prompt:
-            store = False
-            prompt = prompt.replace("--dont-store", "", 1)
-        else:
-            store = True
+        prompt = [message_text.replace("/raw ", "", 1)]
+        store = "--dont-store" not in prompt[0]
+        if not store:
+            prompt[0] = prompt[0].replace("--dont-store", "", 1)
 
-    prompt = [prompt] + photos + additional_media
     model_name = await db.get_chat_parameter(message.chat.id, "model")
 
     api_task = asyncio.create_task(_call_gemini_api(
         request_id,
         prompt,
-        3,
         token,
         model_name
     ))
 
     response = await api_task
     typing_task.cancel()
-    # I have no idea how and why but ok.
     try:
         await typing_task
     except asyncio.CancelledError:
@@ -167,44 +191,13 @@ async def generate_response(message: Message) -> str:
     show_error_message = await db.get_chat_parameter(message.chat.id, "show_error_messages")
 
     try:
-        if isinstance(response, AsyncGenerateContentResponse):
-            output = response.text.replace("  ", " ")
-        elif isinstance(response, str):
-            output = response
-        elif isinstance(response, Exception):
-            error_message = (": " + str(response)) if show_error_message else ""
-            output = f"❌ *Произошел сбой Gemini API{error_message}*"
-        else:
-            logger.error(f"{request_id} | What? {type(response)}")
-            if store:
-                await db.save_system_message(message.chat.id, "Your response was supposed to be here, but you failed "
-                                                              "to reply for some reason. Be better next time.")
-            output = "❌ *Произошел сбой Gemini API.*"
-        if not output.startswith("❌") and store:
-            await db.save_our_message(message, output)
-        return output
+        return await _handle_api_response(request_id, response, message, store, show_error_message)
     except Exception as e:
         logger.error(f"{request_id} | Failed to generate message. Exception: {e}")
-        try:
-            if response.prompt_feedback.block_reason:
-                logger.debug(f"{request_id} | Block reason: {response.prompt_feedback}")
-                if store:
-                    await db.save_system_message(message.chat.id, "Your response was supposed to be here, but you "
-                                                                  "failed to reply for some reason. Be better next time.")
-                return "❌ *Запрос был заблокирован цензурой Gemini API.*"
-            else:
-                if store:
-                    await db.save_system_message(message.chat.id,
-                                                 "Your response was supposed to be here, but you failed to reply for "
-                                                 "some reason. Be better next time.")
-                error_message = (": " + str(e)) if show_error_message else ""
-                return f"❌ *Произошел сбой Gemini API{error_message}.*"
-        except Exception:
-            if store:
-                await db.save_system_message(message.chat.id,
-                                             "Your response was supposed to be here, but you failed to reply for some "
-                                             "reason. Be better next time.")
-            return "❌ *Произошел сбой Gemini API.*"
+        if store:
+            await db.save_system_message(message.chat.id, ERROR_MESSAGES['system_failure'])
+        error_message = (": " + str(e)) if show_error_message else ""
+        return ERROR_MESSAGES['unknown'].format(error_message)
 
 
 async def count_tokens_for_chat(messages_list: list, model_name: str) -> int:
@@ -222,3 +215,16 @@ async def count_tokens_for_chat(messages_list: list, model_name: str) -> int:
         token_count = 0
 
     return token_count
+
+
+def get_available_models() -> list:
+    logger.info("Getting available models...")
+    genai.configure(api_key=_get_api_key())
+    models = genai.list_models()
+    model_list = []
+    hidden = ["bison", "aqa", "embedding", "gecko"]
+    for model in models:
+        if not any(hidden_word in model.name for hidden_word in hidden):
+            model_list.append(model.name.replace("models/", ""))
+
+    return model_list
