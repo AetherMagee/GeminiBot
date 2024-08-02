bceea421d085dbad9e1ebcc81463761ca84a231d
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-02 19:26:21 +0300

    fix(utils): Avoid using just message.text

--- utils/log_command.py
+++ utils/log_command.py
@@ -5,4 +5,4 @@ from loguru import logger
 def log_command(message: Message) -> None:
     if not message.text.startswith("/"):
         return
-    logger.debug(f"{message.from_user.id} | {message.chat.id} | {message.text}")
+    logger.debug(f"{message.from_user.id} | {message.chat.id} | {message.text if message.text else message.caption}")
