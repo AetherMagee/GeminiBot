4e911219ac4aee6644c0adb6a53ab01ec86a59f8
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-06 20:56:22 +0300

    fix(db.msg.edit): Use proper variable for chat id

--- db/messages/edit.py
+++ db/messages/edit.py
@@ -8,7 +8,7 @@ async def replace_message(chat_id: int, message_id: int, text: str):
     san_chat_id = await dbs.sanitize_chat_id(chat_id)
     async with dbs.pool.acquire() as conn:
         try:
-            result = await conn.execute(f"UPDATE messages{chat_id} SET text=$1 WHERE message_id=$2", text, message_id)
+            result = await conn.execute(f"UPDATE messages{san_chat_id} SET text=$1 WHERE message_id=$2", text, message_id)
             if str(result).split(" ")[1] == "1":
                 return True
             else:
