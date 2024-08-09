1e93cb05201242d8b0f481d9df73d3cdb5dc1556
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-09 22:31:00 +0300

    refactor(db.get): Allow custom message limits

--- db/messages/get.py
+++ db/messages/get.py
@@ -5,12 +5,15 @@ import db.shared as dbs
 from db.table_creator import create_message_table
 
 
-async def get_messages(chat_id: int) -> list[Record]:
+async def get_messages(chat_id: int, message_limit: int = None) -> list[Record]:
+    if not message_limit:
+        message_limit = await db.get_chat_parameter(chat_id, 'message_limit')
+
     async with dbs.pool.acquire() as conn:
         try:
             results = await conn.fetch(f"SELECT * FROM messages{await dbs.sanitize_chat_id(chat_id)} "
                                        f"WHERE deleted=false ORDER BY timestamp "
-                                       f"LIMIT {await db.get_chat_parameter(chat_id, 'message_limit')}")
+                                       f"LIMIT {message_limit}")
 
             return results
         except UndefinedTableError:
