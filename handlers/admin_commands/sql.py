54a5f3f7bcbd400ff4d842bf98b246fd49f13312
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-09 23:22:54 +0300

    feat: -fetch for /sql

--- handlers/admin_commands/sql.py
+++ handlers/admin_commands/sql.py
@@ -1,3 +1,4 @@
+from aiogram import html
 from aiogram.types import Message
 
 import db.shared as dbs
@@ -6,11 +7,21 @@ from utils import log_command
 
 async def sql_command(message: Message):
     await log_command(message)
+
+    message_text = message.text
+    fetch = False
+    if "-fetch" in message_text:
+        message_text = message_text.replace(" -fetch", "")
+        fetch = True
+
     try:
-        command = message.text.split(" ", maxsplit=1)
+        command = message_text.split(" ", maxsplit=1)
         async with dbs.pool.acquire() as conn:
-            result = await conn.execute(command[1])
-            await message.reply(str(result))
+            if fetch:
+                result = await conn.fetch(command[1])
+            else:
+                result = await conn.execute(command[1])
+            await message.reply(html.quote(str(result)))
     except Exception as e:
         await message.reply(str(e))
 
