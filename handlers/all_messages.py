7854e8bc66337da2d882b4477ea684110674b3e9
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-14 14:28:20 +0300

    chore: Remove log message for message edit event

--- handlers/all_messages.py
+++ handlers/all_messages.py
@@ -88,5 +88,4 @@ async def handle_new_message(message: Message) -> None:
 
 
 async def handle_message_edit(message: Message) -> None:
-    logger.info(f"Updating message {message.message_id} in {message.chat.id}")
     await db.replace_message(message.chat.id, message.message_id, message.text)
