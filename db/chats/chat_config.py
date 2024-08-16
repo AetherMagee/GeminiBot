5136456c4b3ee9021b7851f9a9e78e646d8f89d0
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-16 19:00:53 +0300

    feat(db): Caching for chat parameters

--- db/chats/chat_config.py
+++ db/chats/chat_config.py
@@ -1,3 +1,5 @@
+from async_lru import alru_cache
+
 import db.shared as dbs
 from utils.definitions import chat_configs
 
@@ -9,6 +11,7 @@ async def _create_config_entry(chat_id: int):
             await conn.execute("INSERT INTO chat_config VALUES ($1)", chat_id)
 
 
+@alru_cache
 async def get_chat_parameter(chat_id: int, parameter_name: str):
     await _create_config_entry(chat_id)
     async with dbs.pool.acquire() as conn:
@@ -27,3 +30,5 @@ async def set_chat_parameter(chat_id: int, parameter_name: str, value):
 
     async with dbs.pool.acquire() as conn:
         await conn.execute(f"UPDATE chat_config SET {parameter_name} = {value} WHERE chat_id = {chat_id}")
+
+    get_chat_parameter.cache_invalidate(chat_id, parameter_name)
