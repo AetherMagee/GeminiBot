145294311c326fd5ee96ea602446dc3dcab2ddca
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-17 10:35:23 +0300

    refactor: Clarify accepted values for frange()

--- utils/frange.py
+++ utils/frange.py
@@ -9,7 +9,6 @@ class FloatRange:
         if self.step == 0:
             raise ValueError("Step cannot be zero")
 
-        # Add a small epsilon to include the stop value
         epsilon = 1e-10
         self.length = max(0, int((self.stop - self.start + epsilon) / self.step) + 1)
 
@@ -26,12 +25,11 @@ class FloatRange:
             raise IndexError("FloatRange index out of range")
 
         value = self.start + index * self.step
-        # Ensure we don't exceed the stop value due to floating point imprecision
         return min(value, self.stop)
 
     def __iter__(self):
         return (self[i] for i in range(len(self)))
 
 
-def frange(*args):
-    return FloatRange(*args)
+def frange(start, stop=None, step=1.0):
+    return FloatRange(start, stop, step)
