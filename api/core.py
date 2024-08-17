0592173f5508512556f8a67e78465438bfc3955f
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-08-17 16:10:50 +0300

    fix(api): Accept endpoint as a parameter instead of looking it up

--- api/core.py
+++ api/core.py
@@ -6,8 +6,7 @@ import api.openai
 import db
 
 
-async def generate_response(message: Message) -> str:
-    endpoint = await db.get_chat_parameter(message.chat.id, "endpoint")
+async def generate_response(message: Message, endpoint: str) -> str:
     if endpoint == "google":
         return await api.google.generate_response(message)
     elif endpoint == "openai":
@@ -19,7 +18,7 @@ async def generate_response(message: Message) -> str:
 
         auto_fallback_allowed = await db.get_chat_parameter(message.chat.id, "o_auto_fallback")
         if out.startswith("❌") and auto_fallback_allowed:
-            logger.debug(f"autofallback {auto_fallback_allowed}")
+            logger.debug(f"Falling back to Google...")
             crash_warning = await message.reply(f"⚠️ <b>Эндпоинт OpenAI дал сбой, запрос был направлен в Gemini API.</b>")
             out = await api.google.generate_response(message)
             await crash_warning.delete()
