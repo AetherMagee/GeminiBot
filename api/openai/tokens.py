e5364dccf0f6bdbc34e2ae314bf7d87bf5305e3f
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-10-07 15:51:30 +0300

    feat: Improve tokenization accuracy

--- api/openai/tokens.py
+++ api/openai/tokens.py
@@ -1,13 +1,30 @@
+import time
+
 import tiktoken
+from loguru import logger
 
 import db
 from api.google import format_message_for_prompt
-
-encoding = tiktoken.get_encoding("cl100k_base")
 
 
 async def count_tokens(chat_id: int) -> int:
     all_messages_list = [await format_message_for_prompt(message) for message in await db.get_messages(chat_id)]
     text = "\n".join(all_messages_list)
 
-    return len(encoding.encode(text))
+    load_encoding_start = time.perf_counter()
+    try:
+        encoding = tiktoken.encoding_for_model(await db.get_chat_parameter(chat_id, "o_model"))
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    load_encoding_end = time.perf_counter()
+    if load_encoding_end - load_encoding_start > 0.5:
+        logger.warning(f"Loading an encoding took {round(load_encoding_end - load_encoding_start, 2)}s")
+
+    encode_start = time.perf_counter()
+    encoded = encoding.encode(text)
+    encode_end = time.perf_counter()
+
+    if encode_end - encode_start > 0.5:
+        logger.warning(f"Encoding took {round(encode_end - encode_start, 2)}s")
+
+    return len(encoded)
