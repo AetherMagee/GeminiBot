febc8c6f2adb51174c439372fc23311ec53805e2
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-10-12 22:12:49 +0300

    cleanup: Remove unused ReturnValueThread

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,4 @@
 from .blacklist import BlacklistFilter
 from .fake_typing import simulate_typing
 from .log_command import log_command
-from .rvthread import ReturnValueThread
 from .text import get_message_text
