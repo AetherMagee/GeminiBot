67da5680c2f870226278cd2d22173fc800f92e3d
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-11-23 22:05:07 +0300

    fix(google): Don't show raw error messages

--- api/google/google.py
+++ api/google/google.py
@@ -157,8 +157,8 @@ async def _handle_api_response(
         if isinstance(response, Exception):
             logger.debug(f"{request_id} | Received an exception: {response}")
             output = "❌ *Произошёл сбой Gemini API.*"
-            if show_error_message:
-                output += f"\n\n{response}"
+            # if show_error_message:
+            #     output += f"\n\n{response}"
 
             return output
 
